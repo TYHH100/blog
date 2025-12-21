d60c374324f63580a40cf15d32eeeb31828745f7
TYHH10 <71576227+TYHH100@users.noreply.github.com>
2025-12-21 14:19:00 +0800

    Site updated: 2025-12-21 14:18:59

--- file/script/ssm/install.sh
+++ file/script/ssm/install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 INSTALL_DIR="/tmp/ssm"
-script_name="addons.sh \
-config.sh \
-game.sh \
-service.sh \
-steam.sh \
-system.sh \
-utils.sh"
-mkdir -p "$INSTALL_DIR"
+script_name="addons.sh config.sh game.sh service.sh steam.sh system.sh utils.sh"
+mkdir -p "$INSTALL_DIR/lib"
 
-curl -sSL https://blog.tyhh10.xyz/file/script/ssm/main.sh -o "$INSTALL_DIR/main.sh"
-curl -sSL https://blog.tyhh10.xyz/file/script/ssm/lib/$script_name -o "$INSTALL_DIR/lib/$script_name"
+# 下载 main.sh
+curl -fL "https://blog.tyhh10.xyz/file/script/ssm/main.sh" -o "$INSTALL_DIR/main.sh"
 
+# 逐个下载 lib 下的脚本
+for f in $script_name; do
+  curl -fL "https://blog.tyhh10.xyz/file/script/ssm/lib/$f" -o "$INSTALL_DIR/lib/$f"
+done
 
-# 赋予执行权限并运行
 chmod +x "$INSTALL_DIR/main.sh"
-exec "$INSTALL_DIR/main.sh" "$@"
+exec sudo "$INSTALL_DIR/main.sh" "$@"
